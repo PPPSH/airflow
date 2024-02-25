986f7953b36c95daca31692f5ac5e5e951d36ea7
PPPSH <dbtn751@gmail.com>
2024-02-26 01:11:01 +0900

    op_kargs 테스트용 dag 업로드

---
+++ dags/dags_python_with_op_kargs.py
@@ -0,0 +1,26 @@
+ 
+from airflow import DAG 
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+
+import random 
+ 
+with DAG(
+    dag_id="dags_python_with_op_kwargs", #airflow에서 보여지는 화면, 파일명이랑 DagID랑 일치하는게 좋음 
+    schedule="30 6 * * *", # 분/시/일/월/요일
+    
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False # 시작일부터 돌릴것이냐(누락본 일자 다 돌릴것이냐)
+    
+) as dag:
+    
+    regist2_t1 = PythonOperator(
+        task_id='regist2_t1',
+        python_callable=regist2,
+        op_args=['pppsh','man','kr','seoul'],
+        op_kwargs={'email':'123@naver.com','phone':'010'}
+    )
+    
+    regist2_t1
