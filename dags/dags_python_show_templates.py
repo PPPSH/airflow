86d62c425a7ab0e12335bdadb6e6611407bf3d86
PPPSH <dbtn751@gmail.com>
2024-02-26 10:45:47 +0900

    python_template dag  업로드

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,26 @@
+
+
+from airflow.models.dag import DAG
+
+import pendulum
+from airflow.decorators import task
+
+
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 2, 20, tz="Asia/Seoul"),
+    catchup=True,
+    
+) as dag:
+    
+    @task(task_id="python_task")
+    def show_templates(**kwarges):
+        from pprint import pprint 
+        pprint(kwarges)
+        
+    show_templates()
+
+    
+    
