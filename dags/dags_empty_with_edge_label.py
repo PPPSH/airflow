b793306adb858d55fc94a519a142a063b4ce3290
PPPSH <dbtn751@gmail.com>
2024-02-27 16:49:00 +0900

    dags_empty edge dags 수정  업로드

--- dags/dags_empty_with_edge_label.py
+++ dags/dags_empty_with_edge_label.py
@@ -1,41 +1,40 @@
 from airflow import DAG
+import pendulum
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.edgemodifier import Label
-from airflow.decorators import task
-import pendulum
-import datetime
 
-with DAG (
-    dag_id='dags_empty_with_edge_label',
-    start_date=pendulum.datetime(2024,2,1, tz='Asia/Seoul'),
+
+with DAG(
+    dag_id="dags_empty_with_edge_label",
     schedule=None,
+    start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
     catchup=False
-) as dag :
+) as dag:
     
-    @task(task_id='empty_1')
-    def empty_1():
-        print('empty_1')
-    
-    @task(task_id='empty_2')
-    def empty_2():
-        print('empty_2')
-    
-    empty_1() >> Label('1과 2사이') >> empty_2()
-    
+    empty_1 = EmptyOperator(
+        task_id='empty_1'
+    )
+
+    empty_2 = EmptyOperator(
+        task_id='empty_2'
+    )
+
+    empty_1 >> Label('1과 2사이') >> empty_2
+
     empty_3 = EmptyOperator(
-        task_id ='empty_3'
+        task_id='empty_3'
     )
-    
+
     empty_4 = EmptyOperator(
-        task_id ='empty_4'
+        task_id='empty_4'
     )
-    
+
     empty_5 = EmptyOperator(
-        task_id ='empty_5'
+        task_id='empty_5'
     )
-    
+
     empty_6 = EmptyOperator(
-        task_id ='empty_6'
+        task_id='empty_6'
     )
-    
-    empty_2() >>Label('start branch') >> [empty_3, empty_4,empty_5]>>Label('end branch') >> empty_6
+
+    empty_2 >> Label('Start Branch') >> [empty_3,empty_4,empty_5] >> Label('End Branch') >> empty_6
