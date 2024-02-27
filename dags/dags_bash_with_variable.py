b8db57fa97ebe8a1f0ffdd72fecf8f83269f45fc
PPPSH <dbtn751@gmail.com>
2024-02-27 09:44:40 +0900

    bash_Variable 수정  dags upload

--- dags/dags_bash_with_variable.py
+++ dags/dags_bash_with_variable.py
@@ -15,7 +15,7 @@ with DAG(
     
     bash_var_1=BashOperator(
         task_id = "bash_var_1",
-        bash_command="echo variable:{var_value}"
+        bash_command=f"echo variable:{var_value}"
     )
     
     bash_var_2 = BashOperator(
