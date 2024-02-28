3c32fffbcd460889c4dd3436f6b41d6b5909470a
PPPSH <dbtn751@gmail.com>
2024-02-28 16:37:49 +0900

    dags_trigger_dags_run dag 수정 업로드

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -1,27 +1,23 @@
- 
-from airflow import DAG 
-import datetime
+from airflow import DAG
 import pendulum
+import datetime
 from airflow.operators.python import PythonOperator
-import random 
- 
+import random
+
 with DAG(
-    dag_id="dags_python_operator", #airflow에서 보여지는 화면, 파일명이랑 DagID랑 일치하는게 좋음 
-    schedule="30 6 * * *", # 분/시/일/월/요일
-    
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False # 시작일부터 돌릴것이냐(누락본 일자 다 돌릴것이냐)
-    
+    catchup=False
 ) as dag:
-    
     def select_fruit():
         fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
         rand_int = random.randint(0,3)
         print(fruit[rand_int])
-        
+
     py_t1 = PythonOperator(
-        task_id = 'py_t1',
+        task_id='py_t1',
         python_callable=select_fruit
-     )    
-    
+    )
+
     py_t1
--- dags/dags_trigger_dag_run_operator.py
+++ dags/dags_trigger_dag_run_operator.py
@@ -1,23 +1,23 @@
-#Package Import
+# Package Import
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import pendulum
 
 with DAG(
-    dag_id = 'dags_trigger_dag_run_operator',
-    start_date=pendulum.datetime(2024,2,1, tz='Asia/Seoul'),
-    schedule='30 9 * * * ',
-    catchup= False
-) as dag :
-    
-    start_task=BashOperator(
-        task_id = 'start_task',
-        bash_command='echo "Start!"',
+    dag_id='dags_trigger_dag_run_operator',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    schedule='30 9 * * *',
+    catchup=False
+) as dag:
+
+    start_task = BashOperator(
+        task_id='start_task',
+        bash_command='echo "start!"',
     )
-    
+
     trigger_dag_task = TriggerDagRunOperator(
-        task_id = 'trigger_dag_task',
+        task_id='trigger_dag_task',
         trigger_dag_id='dags_python_operator',
         trigger_run_id=None,
         execution_date='{{data_interval_start}}',
@@ -26,6 +26,6 @@ with DAG(
         poke_interval=60,
         allowed_states=['success'],
         failed_states=None
-    )
-    
+        )
+
     start_task >> trigger_dag_task
