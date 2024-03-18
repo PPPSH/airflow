38db482721c78a22f5bfbff5e561efb1076dea3a
PPPSH <dbtn751@gmail.com>
2024-03-18 23:06:28 +0900

    email study dag upload

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -7,7 +7,7 @@ from  airflow.operators.email import EmailOperator
 
 with DAG (
     dag_id = "dags_email_operator",
-    schedule="0 0 0 0 0",
+    schedule="0 0 1 1 *",
     start_date=pendulum.datetime(2023,3,1, tz= "Asia/Seoul"),
     catchup=False
 ) as dag : 
