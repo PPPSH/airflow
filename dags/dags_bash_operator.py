d88034cc16b2a76a33f75b34935d4abc4ba8e3db
PPPSH <dbtn751@gmail.com>
2024-05-27 22:52:30 +0900

    bash 수정1

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,30 +1,24 @@
- 
-from airflow import DAG 
+from airflow import DAG
 import datetime
 import pendulum
- 
 from airflow.operators.bash import BashOperator
- 
+
+
 with DAG(
-    dag_id="dags_bash_operator", #airflow에서 보여지는 화면, 파일명이랑 DagID랑 일치하는게 좋음 
-    schedule="0 0 * * *", # 분/시/일/월/요일
+    dag_id="dags_bash_operator",  # ui 화면에서 보이는 DAG 이름 , .py 파일명이랑 dag_id 는 일치 시키는게 좋음 
+    schedule="0 0 * * *", # 분 시 일 월 요일
+    start_date=pendulum.datetime(2021,1,1, tz = "Asia/Seoul"),
+    catchup=False, # 위 시작 일부터 돌릴것이냐(한꺼번에 돌려) = True, 오늘부터 돌릴것이냐 = False 
+) as dag: 
     
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
-    catchup=False, # 시작일부터 돌릴것이냐(누락본 일자 다 돌릴것이냐)
-    
-    #dagrun_timeout=datetime.timedelta(minutes=60),
-    
-    #tags=["example", "example2"],
-    #params={"example_key": "example_value"},
-) as dag:
-     bash_t1 = BashOperator( # 객체명
-        task_id="bash_t1",   # tast id , 객체명이랑 일치 시키는게 좋음 
-        bash_command="echo whoami",
+    bash_t1 = BashOperator(
+        task_id="bash_t1", # UI Graph에서 보이는 이름 , 객체명과 task_id명은 동일하게 작성하는게 편리함
+        bash_command="echo blue sky!",
     )
-     
-     bash_t2 = BashOperator( # 객체명
-        task_id="bash_t2",   # tast id , 객체명이랑 일치 시키는게 좋음 
-        bash_command="echo $HOSTNAME",
+    
+    bash_t2 = BashOperator(
+        task_id="bash_t2", # UI Graph에서 보이는 이름 , 객체명과 task_id명은 동일하게 작성하는게 편리함
+        bash_command="echo green ground!",
     )
-     
-     bash_t1 >> bash_t2
+    
+    bash_t1 >> bash_t2
