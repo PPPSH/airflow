41b467bf2cb7ca2e95ceddd5ea9acd67d5a34bef
PPPSH <dbtn751@gmail.com>
2024-06-02 15:44:31 +0900

    bash 수정1

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -9,8 +9,8 @@ from airflow.decorators import task
 
 with DAG(
     dag_id="dags_python_show_templates",
-    schedule="30 9 * * *",
-    start_date=pendulum.datetime(2024, 1, 5, tz="Asia/Seoul"),
+    schedule="30 0 * * *",
+    start_date=pendulum.datetime(2024, 5, 30, tz="Asia/Seoul"),
     catchup=True,
     
 ) as dag:
