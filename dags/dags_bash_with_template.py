e2a2dbab0e1cd4a409d9059088f3775e91a271f5
PPPSH <dbtn751@gmail.com>
2024-06-02 09:02:16 +0900

    bash 수정1

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -6,25 +6,21 @@ import pendulum
 from airflow.operators.bash import BashOperator
  
 with DAG(
-    dag_id="dags_bash_with_templated", #airflow에서 보여지는 화면, 파일명이랑 DagID랑 일치하는게 좋음 
+    dag_id="dags_bash_with_templated", 
     schedule="0 0 * * *", # 분/시/일/월/요일
     
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 5, 15, tz="Asia/Seoul"),
     catchup=False, # 시작일부터 돌릴것이냐(누락본 일자 다 돌릴것이냐)
     
-    #dagrun_timeout=datetime.timedelta(minutes=60),
-    
-    #tags=["example", "example2"],
-    #params={"example_key": "example_value"},
 ) as dag:
     
-     bash_t1 = BashOperator( # 객체명
-        task_id="bash_t1",   # tast id , 객체명이랑 일치 시키는게 좋음 
+     bash_t1 = BashOperator( 
+        task_id="bash_t1",   
         bash_command='echo "data_interval_end: {{ data_interval_end}} " ',
     )
      
-     bash_t2 = BashOperator( # 객체명
-        task_id="bash_t2",   # tast id , 객체명이랑 일치 시키는게 좋음 
+     bash_t2 = BashOperator( 
+        task_id="bash_t2",   
         env ={
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE' : '{{data_interval_end | ds}}'
