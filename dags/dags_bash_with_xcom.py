1bae7be576e5350f3c21b5ad648ead55d87ae18f
pppsh <dbtn751@gmail.com>
2024-06-04 08:04:32 +0900

    error

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -1,33 +1,28 @@
-from airflow import DAG 
-import datetime
+from airflow import DAG
 import pendulum
+import datetime
 from airflow.operators.bash import BashOperator
-from common.common_func import regist
 
-import random 
- 
 with DAG(
-    dag_id="dags_bash_with_xcom", 
-    schedule="30 6 * * *", # 분/시/일/월/요일
-    
+    dag_id="dags_bash_with_xcom",
+    schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
-    
 ) as dag:
     bash_push = BashOperator(
-        task_id = 'bash_push',
-        bash_command="echo START &&"
-                    "echo XCOM_PUSHED"
-                    "{{ti.xcom_push(key='bash_pushed', value='first_bash_message')}} &&"
-                    "echo COMPLETE"
+    task_id='bash_push',
+    bash_command="echo START && "
+                 "echo XCOM_PUSHED "
+                 "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
+                 "echo COMPLETE"
     )
 
     bash_pull = BashOperator(
-        task_id = 'bash_pull',
-        env = {'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed')}}",
-               'RETURN_VALUE:':"{{ti.xcom_pull(task_ids='bash_push')}}"},
-        bash_command = "echo $PUSHED_VALUE && $RETURN_VALUE",
-        do_xcom_push = False
+        task_id='bash_pull',
+        env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
+            'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
+        do_xcom_push=False
     )
 
     bash_push >> bash_pull
