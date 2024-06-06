5ce3d2462c05ccb9aee62e1c662ea131eb2439f1
PPPSH <dbtn751@gmail.com>
2024-06-06 22:10:49 +0900

    dags x-com study1

---
+++ dags/a_dags_bash_python_with_xcom.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="a_dags_bash_python_with_xcom",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    bash_push =BashOperator(
+        task_id = 'bash_push',
+        bash_command='echo start PUSH_BY_BASH '
+                    '{{ti.xcome.push(key="key1", value=200)}}'
+    )
+    
+    bash_pull = BashOperator(
+        task_id = 'bash_pull',
+        env = {
+            'val1' : '{{ti.xcom_pull(key="key1")}}'
+        } ,
+        bash_command= 'echo val is $val1'
+    )
+    
+    bash_push >> bash_push
