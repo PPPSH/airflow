647483046616af0ff529927273c8314d10e67df4
PPPSH <dbtn751@gmail.com>
2024-06-09 00:15:17 +0900

    python branch study

--- dags/a_dags_branch_python_operator.py
+++ dags/a_dags_branch_python_operator.py
@@ -2,6 +2,7 @@ from airflow import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
+from airflow.decorators import task
 
 with DAG(
     dag_id='a_dags_branch_python_operator',
@@ -9,43 +10,43 @@ with DAG(
     schedule='0 1 * * *',
     catchup=False
 ) as dag:
-    def select_random():
+    @task.branch(task_id = 'upload_task1')
+    def select_item():
         from random import choice
-        
         item_lst = ['1','2','3']
         selected_item = choice(item_lst)
         
-        if selected_item == '1':
+        if select_item == '1':
             return 'task_1'
         
-        else:
-            return ['task_2', 'task_3'] 
-    
+        elif select_item == '2':
+            return 'task_2'
+        else :
+            return 'task_3'
+        
     def common_func(**kwargs):
-        print(kwargs['selected'])
-    
-    python_branch_task = BranchPythonOperator(
-        task_id ='python_branch_task',
-        python_callable= select_random
-    )    
-    
+        ti=kwargs['selected']
+        print(f'selected is : {ti}')
+        
     task_1 = PythonOperator(
-        task_id = 'task_1',
-        python_callable=common_func,
-        op_kwargs= {'selected':'1'}
+        taks_id = 'task_1',
+        op_kwargs= {'selected':'task1'},
+        python_callable=common_func
     )
     
-    task_2 = PythonOperator(
+    task_2 =PythonOperator(
         task_id = 'task_2',
-        python_callable=common_func,
-        op_kwargs= {'selected':'2'}
+        op_kwargs= {'selected':'task2'},
+        python_callable=common_func
     )
     
     task_3 = PythonOperator(
         task_id = 'task_3',
-        python_callable=common_func,
-        op_kwargs={'selected':'3'}
+        op_kwargs={'selected':'task3'},
+        python_callable=common_func
     )
     
-    python_branch_task >>[task_1,task_2,task_3]
+    select_item() >> [task_1,task_2,task_3]
+    
+    
     
