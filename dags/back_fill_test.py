d8fa3d926848637f1deeb0b574fe1e1c7da2da7f
PPPSH <dbtn751@gmail.com>
2024-09-20 22:39:04 +0900

    backfill_test

--- dags/back_fill_test.py
+++ dags/back_fill_test.py
@@ -24,20 +24,22 @@ dag = DAG(
     catchup=False  # 백필을 위한 옵션
 )
 
+
 # Task 1: 텍스트 파일 생성 함수
 def generate_txt(**kwargs):
-    execution_time = kwargs['execution_date'].strftime('%Y-%m-%d-%H-%M')  # 파일명에 시간을 포함하도록 수정
-    date = kwargs['execution_date'].strftime('%Y-%m-%d')  # 파일 내 일자 기록
+    # UTC execution_date를 사용
+    execution_time = kwargs['execution_date'].strftime('%Y-%m-%d-%H-%M')  # 파일명에 시간을 포함하여 저장
+    full_execution_time = kwargs['execution_date'].strftime('%Y-%m-%d-%H-%M')  # 파일 내 기록할 전체 시간
     
     file_name = f"/opt/airflow/files/test1/data_{execution_time}.txt"
     
-    # id, date, random_number 데이터를 생성하여 텍스트 파일로 저장
+    # 파일 생성 및 저장
     with open(file_name, 'w') as f:
-        f.write("id,date,random_number\n")
+        f.write("id,full_execution_time,random_number\n")
         for i in range(1, 11):
             random_number = random.randint(1, 100)  # 1에서 100 사이의 랜덤 숫자 생성
-            f.write(f"{i},{date},{random_number}\n")
-    
+            f.write(f"{i},{full_execution_time},{random_number}\n")    
+  
     print(f"File {file_name} has been generated.")
 
 # PythonOperator로 Task 1 정의
