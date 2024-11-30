38f50dec6e69d06a10ba1377be49a4cf9caeab12
PPPSH <dbtn751@gmail.com>
2024-11-30 23:51:15 +0900

    macors with python upload

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -5,7 +5,7 @@ import datetime, pendulum
 with DAG(
     dag_id='dags_bash_with_macros_eg2',
     schedule='10 0 * * 6#2',
-    start_date=pendulum.datetime(2024,11,20,tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024,11,5,tz='Asia/Seoul'),
     catchup=False
 )as dag:
     # START_DATE : 2주전 월요일, END_DATE: 2주전 토요일
